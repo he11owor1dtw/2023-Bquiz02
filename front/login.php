1a639171638ed6f764ff33f87bda509c19482c08
HelloCodeman <955khm@gmail.com>
2023-12-29 15:21:12 +0800

    完成會員登入功能

--- front/login.php
+++ front/login.php
@@ -7,11 +7,11 @@
         </tr>
         <tr>
             <td class="clo">密碼</td>
-            <td><input type="text" name="pw" id="pw"></td>
+            <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
             <td>
-                <input type="submit" value="登入">
+                <input type="button" value="登入" onclick="login()">
                 <input type="reset" value="清除">
             </td>
             <td>
@@ -20,3 +20,29 @@
         </tr>
     </table>
 </fieldset>
+<script>
+    function login() {
+        $.post('./api/chk_acc.php', {
+            acc: $("#acc").val()
+        }, (res) => {
+            if (parseInt(res) == 0) {
+                alert('查無帳號')
+            } else {
+                $.post('./api/chk_pw.php', {
+                    acc: $("#acc").val(),
+                    pw: $("#pw").val()
+                }, (res) => {
+                    if (parseInt(res) == 1) {
+                        if ($("#acc").val() == 'admin') {
+                            location.href = 'back.php'
+                        } else {
+                            location.href = 'index.php'
+                        }
+                    } else {
+                        alert("密碼錯誤")
+                    }
+                })
+            }
+        })
+    }
+</script>
