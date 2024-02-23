ab32b106010b7ec846198e5f125e29c6093f40f0
HelloCodeman <955khm@gmail.com>
2024-02-23 08:43:37 +0800

    補找回密碼功能

--- front/forget.php
+++ front/forget.php
@@ -1,12 +1,21 @@
 <fieldset style=" margin: auto;text-align: center;">
-    <legend>忘記密碼</legend>
-    <div>請輸入信箱以查詢密碼</div>
-    <div>
-        <input type="text" name="" id="" style="width: 250px;">
-    </div>
-    <div id="result"></div>
-    <div>
-        <input type="submit" value="註冊">
-        <input type="reset" value="清除">
-    </div>
+  <legend>忘記密碼</legend>
+  <div>請輸入信箱以查詢密碼</div>
+  <div>
+    <input type="text" name="email" id="email" style="width: 250px;">
+  </div>
+  <div id="result"></div>
+  <div>
+    <button onclick="forget()">找回密碼</button>
+  </div>
 </fieldset>
+
+<script>
+  function forget() {
+    $.get("./api/forget.php", {
+      email: $("#email").val()
+    }, (res) => {
+      $("#result").text(res)
+    })
+  }
+</script>
